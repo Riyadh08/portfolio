d832ab941a7b5a0a3661491079c155f2fa73faf9
ryad <robiulriyadh66@gmail.com>
2024-10-10 18:19:32 +0600

    second commit

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ ScrollReveal({
  ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
  const typed = new Typed('.multiple-text', {
-    strings: ['Student', 'Programmer', 'Commedian'],
+    strings: ['Student', 'Programmer', 'Problem Solver'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
